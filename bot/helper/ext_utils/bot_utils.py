6aae1c80606b9431d5f1902c4685a8c43066ae95
Demi Minority <90416869+Minoritasia@users.noreply.github.com>
2021-10-10 15:51:11 +0700

    No Italic in Status' File Name

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,7 +92,7 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
+            msg += f"{download.name()} - "
             msg += download.status()
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
